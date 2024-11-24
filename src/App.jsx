d2515c4f8fb3e4b154816e6accb706fcaf3ed55f
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-11-24 21:35:04 +0700

    feat:navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 const App = () => {
   return (
     <>
-      <div>div</div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavbarHeader from '../../components/NavbarHeader';
+
+const Home = () => {
+  return (
+    <>
+      <NavbarHeader />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, odit
+        earum. Iure officiis laudantium voluptatum eum adipisci cupiditate cum
+        dolores, velit, doloremque rem dolor harum magni hic repudiandae
+        consectetur eligendi?
+      </div>
+    </>
+  );
+};
+
+export default Home;

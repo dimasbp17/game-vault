49808d2508b4786395517f56c10c2a927e8bd3fa
dimasbp17 <dimas.prasetyo@students.amikom.ac.id>
2024-12-07 20:37:54 +0700

    feat: about page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,9 +4,9 @@ import NavbarHeader from '../components/NavbarHeader';
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-biru">
+    <div className="flex flex-col min-h-screen text-white bg-biru">
       <NavbarHeader />
-      <main className="flex-grow px-4 lg:px-20">{children}</main>
+      <main className="flex-grow px-4 md:px-10 lg:px-20">{children}</main>
       <Footer />
     </div>
   );
